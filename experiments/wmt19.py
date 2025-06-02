620e06ee7117300abd7433c6ca92dafa560c73a7
Jingyan <wayenvan@outlook.com>
2025-06-02 00:57:09 +0000

    test the wmt19 model

---
+++ experiments/wmt19.py
@@ -0,0 +1,25 @@
+import sys
+import torch
+
+sys.path.append(".")
+
+from modules.fsmt.modeling_fsmt import FSMTForConditionalGeneration
+from transformers import FSMTTokenizer
+
+
+mname = "WayenVan/wmt19-en-de"
+tokenizer = FSMTTokenizer.from_pretrained(mname)
+model = FSMTForConditionalGeneration.from_pretrained(mname)
+
+input = "The world of machine learning is fascinating, isn't it?"
+
+eos_token_id = tokenizer.encode("", return_tensors="pt")
+input_ids = tokenizer.encode(input, return_tensors="pt", add_special_tokens=False)
+encoder_embeddings = model.get_input_embeddings()(
+    torch.cat([input_ids, eos_token_id], dim=-1)
+)
+
+outputs = model.generate(inputs_embeds=encoder_embeddings)
+
+decoded = tokenizer.decode(outputs[0], skip_special_tokens=True)
+print(decoded)  # Maschinelles Lernen ist großartig, oder?
